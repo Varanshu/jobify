4ca8c59aa506734748dd61ff16d3670d6a743520
varanshu-agrawal <varanshu.agrawal@digitup.in>
2023-05-01 16:36:24 +0530

    Error page completion

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage'
+
+import NotFoundImage from '../assets/images/not-found.svg'
 
 const Error = () => {
     return (
-        <div>
-            Error
-        </div>
+        <Wrapper className='full-page'>
+            <div>
+                <img src={NotFoundImage} alt='not found' />
+                <h3>Not Found</h3>
+                <p>404 Error</p>
+                <Link to="/">Back Home</Link>
+            </div>
+        </Wrapper>
     )
 }
 
